e5220633e784ef49404cf5f1c4ab845ac448d49b
Spekzwilly <weitihuang@outlook.com>
2018-10-18 14:44:43 +0800

    added project and kaggle machine learning jupyter notebook

--- Employee Churn/main.py
+++ Employee Churn/main.py
@@ -1,16 +1,2 @@
-import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-%matplotlib inline
-
-# Exploratory Analysis
-"""
-Exploratory Data Analysis is an initial process of analysis,
-in which you can summarize characteristics of data such as pattern, trends, outliers,
-and hypothesis testing using descriptive statistics and visualization.
-"""
-
-data = pd.read_csv('HR_comma_sep.csv')
-
-data.head()
-
