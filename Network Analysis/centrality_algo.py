adf73a7c94f9b37a590f8f41d73311cf310fb91f
Spekzwilly <weitihuang@outlook.com>
2018-12-09 13:45:00 +0800

    added some new projects

---
+++ Network Analysis/centrality_algo.py
@@ -0,0 +1,46 @@
+# Degree centrality
+
+def find_nodes_with_highest_deg_cent(G):
+
+    # Compute the degree centrality of G: deg_cent
+    deg_cent = nx.degree_centrality(G)
+
+    # Compute the maximum degree centrality: max_dc
+    max_dc = max(list(deg_cent.values()))
+
+    nodes = set()
+
+    # Iterate over the degree centrality dictionary
+    for k, v in deg_cent.items():
+
+        # Check if the current value has the maximum degree centrality
+        if v == max_dc:
+
+            # Add the current node to the set of nodes
+            nodes.add(k)
+
+    return nodes
+
+
+# Betweenness centrality
+
+def find_node_with_highest_bet_cent(G):
+
+    # Compute betweenness centrality: bet_cent
+    bet_cent = nx.betweenness_centrality(G)
+
+    # Compute maximum betweenness centrality: max_bc
+    max_bc = max(list(bet_cent.values()))
+
+    nodes = set()
+
+    # Iterate over the betweenness centrality dictionary
+    for k, v in bet_cent.items():
+
+        # Check if the current value has the maximum betweenness centrality
+        if v == max_bc:
+
+            # Add the current node to the set of nodes
+            nodes.add(k)
+
+    return nodes
